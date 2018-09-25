d3f87293b33bb0c05d24c587b82ac3660db6255e
SuperWinter111 <1169223634@qq.com>
2018-09-25 22:40:59 +0800

    eee

--- Sketch2codeTest/ailab-master/Sketch2Code/Sketch2Code.Tests/Properties/AssemblyInfo.cs
+++ Sketch2codeTest/ailab-master/Sketch2Code/Sketch2Code.Tests/Properties/AssemblyInfo.cs
@@ -3,10 +3,4 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 
-[assembly: ComVisible(false)]
 
-[assembly: Guid("7d128c34-e235-4789-8508-f66aea3442f1")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
